65b0d7bb244cb494a64ec037c93c5bafd1949b4d
Abin S chandran <abinschandran1@gmail.com>
2026-01-10 12:31:15 +0530

    user created successfully

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -12,7 +12,7 @@ import {
   toggleBlockUser,
   deleteUser,
 } from "../controllers/adminController.js";
-import { verifyAdmin } from "../middlewares/verifyAdmin.js";
+import { verifyAdmin } from "../middleware/adminMiddleware.js";
 
 const router = express.Router();
 
@@ -21,22 +21,17 @@ router.post("/refresh-Admin", refreshAdminToken);
 router.post("/logout-Admin", adminLogout);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
-// Create user
+
 router.post("/create-user", verifyAdmin, createUser);
 
-// View all users
 router.get("/get-users", verifyAdmin, getUsers);
 
-// View single user
 router.get("/get-user/:id", verifyAdmin, getUserById);
 
-// Update user
 router.put("/update-user/:id", verifyAdmin, updateUser);
 
-// Block / Unblock user
 router.patch("/block-user/:id", verifyAdmin, toggleBlockUser);
 
-// Delete user (soft delete)
 router.delete("/delete-user/:id", verifyAdmin, deleteUser);
 
 export default router;
